529755d0e951f7c42ebf2df23d8cf202bf01e443
joacoloza <joaquinciappa@outlook.com>
2025-01-29 12:30:36 -0300

    clerk webhook changed

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 const isPublicRoute = createRouteMatcher([
   '/sign-in(.*)', 
   '/sign-up(.*)',
-  'api/webhooks/clerk(.*)',])
+  'api/webhooks/clerk',])
 
 export default clerkMiddleware(async (auth, request) => {
   if (!isPublicRoute(request)) {
